1ef5ff714d71c958c988cba8a087fc30c5348752
olahkrisz1 <olahkrisz1@gmail.com>
2023-06-21 21:43:13 +0200

    last commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,20 @@
-const TodoList = () => {
+import Todo from "./Todo";
+
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
   return (
     <div className="todo-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            todo={todo}
+            text={todo.text}
+            id={todo.id}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
